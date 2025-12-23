4ad74dee0f821c2a8667353f23555f930ec43957
patricweb <matejtradir@gmail.com>
2025-12-23 16:12:47 +0200

    bug fix

---
+++ components/AIEmailParser.tsx
@@ -0,0 +1,13 @@
+// components/AIEmailParser.tsx
+import React from 'react';
+
+const AIEmailParser: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">AI Email Parser</h1>
+      <p>This is a placeholder component.</p>
+    </div>
+  );
+};
+
+export default AIEmailParser;
